76cc90174072a3de997a45f093458047fdc24a4c
Weizhe (Wesley) Chen <chenweiz@iu.edu>
2021-12-05 18:51:39 -0500

    Added install.sh and updated readme

--- README.md
+++ README.md
@@ -1,42 +1,15 @@
-# Neovim Configuration for C++ and Python
+# NeoVim Configuration for ROS Development with C++ and Python
 
-1. ```bash
-   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-   chmod u+x nvim.appimage
-   sudo mv nvim.appimage /bin/vim
-   
-   sudo apt install xsel clang-format ctags
-   
-   sudo apt-get install python-pip
-   sudo apt-get install python3-pip
-   pip install --upgrade pip
-   pip3 install --upgrade pip
-   pip install neovim unidecode
-   pip3 install neovim unidecode
-   
-   # We might need to install curl first
-   curl -sL install-node.now.sh/lts | sudo bash
-   sudo npm -g install neovim
-   
-   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-          
-   mkdir -p ~/.config/nvim/plugged/
-   cd ~/clone/neovim-cpp-python
-   cp ./init.vim ~/.config/nvim/
-   
-   vim +PlugInstall
-   
-   CocInstall coc-pyright coc-git coc-cmake coc-json coc-vimtex coc-clangd coc-snippets
-   CocCommand clangd.install
-   # Add the following line to ~/.bashrc
-   export PATH=$PATH:/home/chenweiz/.config/coc/extensions/coc-clangd-data/install/12.0.1/clangd_12.0.1/bin
-   ```
+## Get Started
 
-2. Backward search in Okular
+```bash
+./install.sh
+```
 
-   1. "Settings > Editor > Custom Text Editor"
-   2. `nvr --remote-silent +%l %f`
-   3. Might need to install `pip install neovim-remote`
-   4. Shift + left click brings the cursor in the editor to the corresponding location
-   5. Fix Ultisnips expand error: `pip install unidecode`
+## Backward search in Okular
+
+1. "Settings > Editor > Custom Text Editor"
+2. `nvr --remote-silent +%l %f`
+3. Might need to install `pip install neovim-remote`
+4. Shift + left click brings the cursor in the editor to the corresponding location
+5. Fix Ultisnips expand error: `pip install unidecode`
--- install.sh
+++ install.sh
@@ -1,51 +1,105 @@
-################
-# apt installs #
-################
-sudo apt update
-sudo apt install curl  # Download neovim
-sudo apt install xsel  # For accessing the system clipboard
-sudo apt install clang-format-12  # For C++ auto-formatting
-sudo apt install clangd-12  # C++ language server
-sudo apt-get install automake  # Required for building universal-ctags from source
-
-##################
-# Install NeoVim #
-##################
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo mv nvim.appimage /bin/vim
-
-###########################
-# Install universal-ctags #
-###########################
+#######################
+# System dependencies #
+#######################
+echo "========================================================================="
+echo "Update apt..."
+echo "========================================================================="
+sudo apt-get update -y
+echo "========================================================================="
+echo "Install curl..."
+echo "========================================================================="
+sudo apt-get install curl -y  # Download neovim
+echo "========================================================================="
+echo "Install xsel..."
+echo "========================================================================="
+sudo apt-get install xsel -y  # For accessing the system clipboard
+echo "========================================================================="
+echo "Install clang-format..."
+echo "========================================================================="
+sudo apt-get install clang-format-12 -y  # For C++ auto-formatting
+echo "========================================================================="
+echo "Install clangd..."
+echo "========================================================================="
+sudo apt-get install clangd-12 -y  # C++ language server
+echo "========================================================================="
+echo "Install automake..."
+echo "========================================================================="
+sudo apt-get install automake -y  # Required for building universal-ctags from source
+echo "========================================================================="
+echo "Clone universal-ctags..."
+echo "========================================================================="
 git clone https://github.com/universal-ctags/ctags.git
 cd ctags
+echo "========================================================================="
+echo "universal-ctags autogen.sh..."
+echo "========================================================================="
 ./autogen.sh
+echo "========================================================================="
+echo "universal-ctags configure..."
+echo "========================================================================="
 ./configure
+echo "========================================================================="
+echo "universal-ctags make..."
+echo "========================================================================="
 make
+echo "========================================================================="
+echo "universal-ctags install..."
+echo "========================================================================="
 sudo make install
 
-#############################
-# Python support for NeoVim #
-#############################
-sudo apt-get install python3-pip
+##########
+# NeoVim #
+##########
+echo "========================================================================="
+echo "Download NeoVim..."
+echo "========================================================================="
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+echo "========================================================================="
+echo "Install Neovim..."
+echo "========================================================================="
+chmod u+x nvim.appimage
+sudo mv nvim.appimage /bin/vim
+echo "========================================================================="
+echo "Install python3-pip..."
+echo "========================================================================="
+sudo apt-get install python3-pip -y
+echo "========================================================================="
+echo "Update pip3..."
+echo "========================================================================="
 pip3 install --upgrade pip
+echo "========================================================================="
+echo "Install neovim and unidecode..."
+echo "========================================================================="
 pip3 install neovim unidecode
-
-################
-# Install node #
-################
-curl -sL install-node.now.sh/lts | sudo bash
+echo "========================================================================="
+echo "Install node..."
+echo "========================================================================="
+curl -sL install-node.now.sh/lts | sudo bash | y
+echo "========================================================================="
+echo "Node install neovim..."
+echo "========================================================================="
 sudo npm -g install neovim
 
-####################
-# Install vim-plug #
-####################
+#################################
+# Vim Configuration and Plugins #
+#################################
+echo "========================================================================="
+echo "Install vim-plug..."
+echo "========================================================================="
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-###############################
-# Copy vim configuration file #
-###############################
+echo "========================================================================="
+echo "Copy vim configuration file..."
+echo "========================================================================="
 mkdir -p ~/.config/nvim/plugged/
 cd ~/clone/neovim-cpp-python
 cp ./init.vim ~/.config/nvim/
+echo "========================================================================="
+echo "Install vim plugins..."
+echo "========================================================================="
+vim +'PlugInstall' +qall
+echo "========================================================================="
+echo "Install coc extensions..."
+echo "========================================================================="
+vim +'CocInstall -sync coc-pyright coc-git coc-cmake coc-json coc-vimtex coc-clangd coc-snippets
+CocCommand clangd.install' +qall
+vim +CocUpdateSync +qall
